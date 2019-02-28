8c2f3c35fac52bcdda3fa31fd9f652af4d06a527
sneddybob <bobsned12@gmail.com>
2019-02-28 10:40:56 -0600

    firbase interaction > call last stored color and and set it on load

--- app1.js
+++ app1.js
@@ -1,2 +1,21 @@
+var body = document.querySelector("body");
 
+var colors = ["red", "orange", "yellow", "green", "blue", "indigo", "violet"];
+
+window.firebase.database().ref('/color').once('value').then(function(snapshot){
+    body.style.backgroundColor = snapshot.val();
+})
+
+body.addEventListener("click", function(e){
+    var currentColor = body.style.backgroundColor;
+    var currentColorPosition = colors.indexOf(currentColor);
+    var newColorName;
+    if((currentColorPosition == -1) || (currentColorPosition == (colors.length - 1))){
+        newColorName = colors[0];
+    } else{
+        newColorName = colors[currentColorPosition + 1];
+    }
+    window.firebase.database().ref('/color').set(newColorName);
+    body.style.backgroundColor = newColorName;
+});
     
