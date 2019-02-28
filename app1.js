155d6f59cd64b4f999c6c16b6783894f24dffbc2
sneddybob <bobsned12@gmail.com>
2019-02-28 12:05:44 -0600

    working auth page

--- app1.js
+++ app1.js
@@ -1,10 +1,12 @@
 var body = document.querySelector("body");
 
+var u;
+
 var colors = ["red", "orange", "yellow", "green", "blue", "indigo", "violet"];
 
-window.firebase.database().ref('/color').once('value').then(function(snapshot){
+function initializeColor(){window.firebase.database().ref('/' + u.uid + 'color').once('value').then(function(snapshot){
     body.style.backgroundColor = snapshot.val();
-})
+});};
 
 body.addEventListener("click", function(e){
     var currentColor = body.style.backgroundColor;
@@ -15,7 +17,17 @@ body.addEventListener("click", function(e){
     } else{
         newColorName = colors[currentColorPosition + 1];
     }
-    window.firebase.database().ref('/color').set(newColorName);
+    window.firebase.database().ref('/' + u.uid + '/color').set(newColorName);
     body.style.backgroundColor = newColorName;
 });
     
+
+firebase.auth().onAuthStateChanged(function(user) {
+    if (user){
+            u = user;
+            initializeColor();
+    }
+    else{
+        window.location.href = "./auth.html";
+    }
+});
